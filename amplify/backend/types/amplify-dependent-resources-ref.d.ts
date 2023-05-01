f6977d39749af46688343a5cada04eb2b611d3c4
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-05-01 08:04:10 -0500

    amplify publish
    Antivirus program flagged a file as a Trojan horse. I hope it's not going to affect the app overall

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ccrawfordproject6d51f2765": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
